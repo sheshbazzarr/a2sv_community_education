4d815ab3165af7b926aea32b96e564c142652887
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-12 05:23:34 -0500

    Create 179. Largest Number.py

---
+++ div 3/179. Largest Number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        str_list=[]
+        for ent in nums:
+            str_list+=[str(ent)]
+        L=len(str_list)
+        for sLw in range(L):
+            for fAs in range(sLw+1,L):
+                ascend=str_list[sLw]+str_list[fAs]
+                descend=str_list[fAs]+str_list[sLw]
+                
+                if ascend > descend:
+                    continue
+                else:
+                    str_list[sLw],str_list[fAs]=str_list[fAs],str_list[sLw]
+        Number= "".join(str_list)
+        if int(Number)==0:
+            return '0'
+        return Number
