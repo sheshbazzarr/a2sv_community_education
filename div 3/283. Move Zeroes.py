5bb3c6ac463493a6f2996d3684d1702080e049e3
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-14 05:27:59 -0500

    Create 283. Move Zeroes.py

---
+++ div 3/283. Move Zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        right=len(nums)-1
+        while right >left:
+            if nums[left]==0:
+                nums.pop(left)
+                nums.append(0)
+                right-=1
+            else:
+                left+=1
+      
