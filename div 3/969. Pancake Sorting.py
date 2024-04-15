b2f176dfb137b5ab1a6f4af3db1ad4e9664b3eb9
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-04-15 02:15:16 -0500

    Create 969. Pancake Sorting.py

---
+++ div 3/969. Pancake Sorting.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        res = []
+        for i in range(n):
+            cur_max = max(arr[0:n-i])
+            j = 0
+            while arr[j] != cur_max:
+                j += 1
+            # should reverse j+1 elements
+            arr[:j+1] = reversed(arr[:j+1])
+            res.append(j+1)
+            # reverse all
+            arr[:n-i] = reversed(arr[:n-i])
+            res.append(n-i)
+        return res
