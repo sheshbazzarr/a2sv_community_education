fb8dd5dc3a49a9c4fe716cd73c36bc3067f8fa74
danieldemerw <demerwdani@gmail.com>
2024-04-24 03:57:18 -0500

    commit 1893.check if all the integers in a range are covered

---
+++ div 2/1893.check all the integers in Range are covered.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCovered(self, ranges, left: int, right: int):
+        inclusive=set()  
+        for  i,j in ranges:
+            for k in range(i,j+1):
+                inclusive.add(k)    
+        for i in range(left,right+1):
+            if i not in inclusive:
+                return False
+            return True
+
+
+
+        
