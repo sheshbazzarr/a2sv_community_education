1b96880ca8c8c6f1f322c3979cb0ff234e6c6ec9
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-05-28 09:01:06 -0500

    Create 2069. Walking Robot Simulation II.py
    https://leetcode.com/problems/walking-robot-simulation-ii/

---
+++ div 2/2069. Walking Robot Simulation II.py
@@ -0,0 +1,46 @@
+class Robot:
+    def __init__(self, width: int, height: int):
+        self.w = width
+        self.h = height
+        self.x = 0
+        self.y = 0
+        self.dir = "East"
+        
+
+    def step (self, num: int) -> None:
+        num %= self.w * 2 + self.h * 2 - 4
+        if num == 0:
+            num = self.w * 2 + self.h * 2 - 4
+        
+        for _ in range(num):
+            if self.dir == "East":
+                if self.x == self.w - 1:
+                    self.dir = "North"
+                    self.y += 1
+                else:
+                    self.x += 1
+            elif self.dir == "North":
+                if self.y == self.h - 1:
+                    self.dir = "West"
+                    self.x -= 1
+                else:
+                    self.y += 1
+            elif self.dir == "West":
+                if self.x == 0:
+                    self.dir = "South"
+                    self.y -= 1
+                else:
+                    self.x -= 1
+            elif self.dir == "South":
+                if self.y == 0:
+                    self.dir = "East"
+                    self.x += 1
+                else:
+                    self.y -= 1
+
+    def getPos(self) -> list:
+        return [self.x, self.y]
+
+    def getDir(self) -> str:
+        return self.dir
+
