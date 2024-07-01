da509c483ad478b58df66535f27e42c773067c11
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-07-01 04:20:18 -0500

    Create 350. Intersection of Two Arrays II.py

---
+++ div 3/350. Intersection of Two Arrays II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i,j=0,0
+        nums1,nums2=sorted(nums1),sorted(nums2)
+        output=[]
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                output.append(nums1[i])
+                j+=1
+                i+=1
+        return output
+
