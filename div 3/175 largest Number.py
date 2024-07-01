de4cdcb7958b18d0fab664bb3ebea0d7c47f3b7a
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-07-01 07:48:27 -0500

    Create 175 largest Number.py

---
+++ div 3/175 largest Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i ,n in enumerate(nums):
+            nums[i]=str(n)
+        def compare(n1,n2):
+            if n1+n2>n2+n1:
+                return -1 #first first
+            else:
+                return 1 #second first 
+        nums=sorted(nums,key=cmp_to_key(compare))
+        return str(int("".join(nums)))
