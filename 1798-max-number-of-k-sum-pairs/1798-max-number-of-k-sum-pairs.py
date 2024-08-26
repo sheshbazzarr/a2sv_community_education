37af13757c3153f4dfc1b5901d3c17fcdec7974b
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-08-26 13:23:29 -0500

    Time: 499 ms (41.04%), Space: 28.9 MB (60.35%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        count=0
-        map_freqency=defaultdict(int)
-        for num in nums:
-            compliment = k-num
-            if map_freqency[compliment] > 0:
-                count+=1
-                map_freqency[compliment]-=1
-
+        nums.sort()
+        left_ptr=0
+        right_ptr=len(nums)-1
+        operation_count=0
+        while left_ptr<right_ptr:
+            current_sum=nums[left_ptr]+nums[right_ptr]
+            if current_sum==k:
+                operation_count+=1
+                left_ptr+=1
+                right_ptr-=1
+            elif current_sum<k:
+                left_ptr+=1
             else:
-                map_freqency[num]+= 1
-        return count
+                right_ptr-=1
+        return operation_count
