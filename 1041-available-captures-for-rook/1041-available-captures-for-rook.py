bd9bff13f6de6d0dd7de3f3226d103d8b46aa23c
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-08-27 14:57:30 -0500

    Time: 34 ms (78.31%), Space: 16.6 MB (45.5%) - LeetHub

--- 1041-available-captures-for-rook/1041-available-captures-for-rook.py
+++ 1041-available-captures-for-rook/1041-available-captures-for-rook.py
@@ -1,45 +1,45 @@
 class Solution:
     def numRookCaptures(self, board: List[List[str]]) -> int:
-        # Finding the rook's position
+        # Find the rook's position
         for i in range(8):
             for j in range(8):
-                if board[i][j] == 'R':
+                if board[i][j] == "R":
                     rook_row, rook_col = i, j
                     break
 
-        # Initialize count of attacked pawns
-        pawn_count = 0
+        # Initialize the count of attacked pawns
+        attack_pawn = 0
 
-        # Check upward direction
-        for r in range(rook_row - 1, -1, -1):
-            if board[r][rook_col] == 'p':
-                pawn_count += 1
+        # Check right
+        for i in range(rook_col + 1, 8):
+            if board[rook_row][i] == "p":
+                attack_pawn += 1
                 break
-            elif board[r][rook_col] == 'B':
-                break
-
-        # Check downward direction
-        for r in range(rook_row + 1, 8):
-            if board[r][rook_col] == 'p':
-                pawn_count += 1
-                break
-            elif board[r][rook_col] == 'B':
+            elif board[rook_row][i] == "B":
                 break
 
-        # Check leftward direction
-        for c in range(rook_col - 1, -1, -1):
-            if board[rook_row][c] == 'p':
-                pawn_count += 1
+        # Check left
+        for i in range(rook_col - 1, -1, -1):
+            if board[rook_row][i] == "p":
+                attack_pawn += 1
                 break
-            elif board[rook_row][c] == 'B':
-                break
-
-        # Check rightward direction
-        for c in range(rook_col + 1, 8):
-            if board[rook_row][c] == 'p':
-                pawn_count += 1
-                break
-            elif board[rook_row][c] == 'B':
+            elif board[rook_row][i] == "B":
                 break
 
-        return pawn_count
+        # Check upward
+        for j in range(rook_row - 1, -1, -1):
+            if board[j][rook_col] == "p":
+                attack_pawn += 1
+                break
+            elif board[j][rook_col] == "B":
+                break
+
+        # Check downward
+        for j in range(rook_row + 1, 8):
+            if board[j][rook_col] == "p":
+                attack_pawn += 1
+                break
+            elif board[j][rook_col] == "B":
+                break
+
+        return attack_pawn
