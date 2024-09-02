73cf179039666f0297fe95fc2707ce6d4a115f75
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-09-02 18:56:50 -0500

    Time: 502 ms (35.53%), Space: 29 MB (43.92%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,24 +1,19 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        freq_map = defaultdict(int)
-        
-        # Count frequencies of each number
-        for num in nums:
-            freq_map[num] += 1
-
+        nums.sort()  
         count = 0
+        i = 0
+        j = len(nums) - 1
         
-        for num in list(freq_map.keys()):  # Iterate over a copy of keys to avoid modifying the dictionary during iteration
-            complement = k - num
-            if complement in freq_map:
-                if num == complement:
-                    # Special case: num == complement (e.g., k = 6, num = 3)
-                    count += freq_map[num] // 2
-                else:
-                    # Regular case: num != complement
-                    pairs = min(freq_map[num], freq_map[complement])
-                    count += pairs
-                    freq_map[num] -= pairs
-                    freq_map[complement] -= pairs
-                    
+        while i < j:
+            current_sum = nums[i] + nums[j]
+            if current_sum == k:
+                count += 1
+                i += 1
+                j -= 1
+            elif current_sum < k:
+                i += 1
+            else:
+                j -= 1
+                
         return count
