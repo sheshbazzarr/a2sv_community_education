fb0835463e3ed905c85ced96ad2dc5c4510f7b68
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-09-03 03:57:56 -0500

    Time: 50 ms (64.77%), Space: 16.5 MB (97.95%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,11 +1,19 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows ==1: return s
-        res = ""
-        for r in range(numRows):
-            increment = 2*(numRows-1)
-            for i in range(r,len(s),increment):
-                res+=s[i]
-                if(r>0 and r < numRows-1 and i + increment -2*r < len(s)):
-                    res +=s[i+increment -2 * r]
-        return res            
+    def convert(self,s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        # Create an array of strings, one for each row
+        rows = [''] * numRows
+        cur_row = 0
+        going_down = False
+
+        for char in s:
+            rows[cur_row] += char
+            # Change direction when you reach the top or bottom row
+            if cur_row == 0 or cur_row == numRows - 1:
+                going_down = not going_down
+            cur_row += 1 if going_down else -1
+
+        # Combine all rows to form the final output
+        return ''.join(rows)
