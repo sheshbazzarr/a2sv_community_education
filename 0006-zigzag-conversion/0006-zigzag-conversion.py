c2655c65e354a7a722fccb3b27aa070f0c830f9b
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-09-03 06:02:27 -0500

    Time: 60 ms (23.87%), Space: 16.5 MB (85.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,15 +1,29 @@
 class Solution:
     def convert(self,s: str, numRows: int) -> str:
-        if numRows==1 or len(s)<=numRows:
+        if numRows == 1 or len(s) <= numRows:
             return s
-        rows=['']*numRows
-        cur_row=0
-        can_I_go_down=False
+        
+        # Initialize an array of strings to represent each row
+        rows = [''] * numRows
+        cur_row = 0
+        moving_down = True  # Initialize direction as moving downwards
         
         for chr in s:
-            rows[cur_row]+=chr
-            # you can change dir
-            if cur_row ==0 or cur_row==numRows-1:
-                can_I_go_down = not can_I_go_down
-            cur_row+=1 if can_I_go_down else -1
+            rows[cur_row] += chr
+            
+            # Determine if we need to change direction
+            if cur_row == 0:
+                # If we're at the top row, we should start moving downwards
+                moving_down = True
+            elif cur_row == numRows - 1:
+                # If we're at the bottom row, we should start moving upwards
+                moving_down = False
+            
+            # Move the current row pointer based on the direction
+            if moving_down:
+                cur_row += 1  # Move down to the next row
+            else:
+                cur_row -= 1  # Move up to the previous row
+
+        # Combine all the rows to form the final converted string
         return ''.join(rows)
