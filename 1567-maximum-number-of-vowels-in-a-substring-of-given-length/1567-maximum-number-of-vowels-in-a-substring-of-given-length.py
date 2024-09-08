1f4bfe07477b6301124befc95c9b5a8ebb550057
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-09-08 17:15:54 -0500

    Time: 117 ms (62.27%), Space: 17.4 MB (17.52%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,18 +1,17 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = set('aeiou')
-        max_vowels = 0
-        current_vowels =0
-        
-        for i in range(k):
-            if s[i] in vowels:
-                current_vowels+=1
-        max_vowels=current_vowels
-        # Slide the window across the string
-        for i in range(k,len(s)):
-            if s[i] in vowels:
-                current_vowels+=1
+        vowels = {'a','e','i','o','u'}
+        current_vowel_count = sum (1 for i in range(k) if s[i] in vowels)
+        max_vowel_count =  current_vowel_count
+
+        for  i in range(k, len(s)):
+
             if s[i-k] in vowels:
-                current_vowels -=1
-            max_vowels=max(max_vowels,current_vowels)
-        return max_vowels
+                current_vowel_count-=1
+                # add the curacter that is sliding into the window
+            if s[i] in vowels:
+                current_vowel_count +=1
+            
+            max_vowel_count = max(max_vowel_count,current_vowel_count)
+        return max_vowel_count
+            
