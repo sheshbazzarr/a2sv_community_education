00bb21eb18873b636a9b2c82168baa0baf9b00f5
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-10-23 00:14:36 -0500

    Time: 20 ms (98.64%), Space: 22.3 MB (99.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -9,11 +9,8 @@ class Solution:
         for i in range(n):
             result[i]=left_prod
             left_prod*=nums[i]
-        
         right_prod=1
         for i in range(n-1,-1,-1):
-            result[i]*=right_prod
-            right_prod*=nums[i]
-        
+            result[i]=right_prod*result[i]
+            right_prod=right_prod*nums[i]
         return result
-    
