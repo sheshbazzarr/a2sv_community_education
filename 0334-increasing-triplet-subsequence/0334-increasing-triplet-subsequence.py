56c981829118cfcb58bc9b7ffe912da218cbdbef
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-10-23 01:56:54 -0500

    Time: 12 ms (99.51%), Space: 36.4 MB (7.22%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,13 +1,12 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        #  the game is about finding smallest and second smallest number
-        # infit
-        first = second = float('inf')
-        for num in nums:
-            if num<=first:   # doest the first greater than the number
-                first =num
-            elif num<=second:
-                second =num
+        first=second = float('inf')
+        for num in  nums:
+            if num<=first:
+                first=num
+            elif num <=second:
+                second = num
             else:
-                return True  # if there is a third then it is right 
+               return  True
         return False
+            
