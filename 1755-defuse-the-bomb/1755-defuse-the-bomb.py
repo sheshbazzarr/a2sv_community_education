3f3ee2e6847c66624337c90f66311616c3432748
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-15 05:19:42 -0500

    Time: 8 ms (19.21%), Space: 16.7 MB (59.51%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -3,19 +3,17 @@ from typing import List
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         n = len(code)
+        COPYCODE=[0]*n
         if k==0:
-            for i in range(n):
-                code[i]=0
-            return code
-        COPYCODE=code[:] # O(n) ---->>
+            return COPYCODE 
          
         if k>0:
             for i in range(n):
-                code[i]=sum(COPYCODE[(i+j)%n] for j in range(1,k+1))
+                COPYCODE[i]=sum(code[(i+j)%n] for j in range(1,k+1))
         else:
             for i in range(n):
-                code[i]=sum(COPYCODE[(i+j)%n] for j in range(k,0))
-        return code
+                COPYCODE[i]=sum(code[(i+j)%n] for j in range(k,0))
+        return COPYCODE
 
         # time complexity O(n)
         # space  0(N)
