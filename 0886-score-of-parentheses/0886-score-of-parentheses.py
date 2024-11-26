78fa006a0cd36b9a1d27a1be0996b4fe59f06128
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-26 10:07:07 -0500

    Time: 1 ms (7.22%), Space: 17 MB (6.08%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.py
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -1,20 +1,45 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        #
-        stack =  []
-        output,val= 0,0
-        for p in s:
-            if p =="(":
+        """ 
+            Calculate the score of a balanced parentheses string.]
+            
+            Rules:
+            - "()" has a score of 1.
+            - "AB"(wher A and B are valid parentheses strings) has a score of A+B.     
+            - "(A)" has a score of 2*A.
+
+
+            Args:
+            s (str):The input string consisting of '('  and ')'.
+
+            Returns:
+            int: The score of the parentheses string.
+        """
+            #    Stack to store intermediate scores 
+        stack=[]
+        # The final output score 
+        output = 0
+        # Iterate through each character in the string
+        for char in s:
+            if char =="(":
+                # Push a zero onto the stack to represent the start of a new scope
                 stack.append(0)
-            elif p==")":
-                multiplier=stack.pop()
-                if multiplier ==0:
-                    val =1
+            elif char ==")":
+                # Pop the top value from the stack 
+                top = stack.pop()
+
+                # if the top value is zero , it means the scop is "()" , which has a score of 2
+                if top == 0:
+                    score =1
                 else:
-                    val = multiplier*2
+                    # Otherwise, double the score of the inner scope
+                    score = top*2
                 if not stack:
-                    output+=val   
+                    # if the stack is empty, it means this score belongs 
+                    # to the outermost level
+                    output+=score
                 else:
-                    stack[-1]+=val
-        
+                    # Otherwise, add this score to the last element in the stack
+                    stack[-1]+=score
         return output
+
