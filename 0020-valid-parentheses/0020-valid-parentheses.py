0e6abff67b2fa1367d951bb2267eb1d01decddc9
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-26 14:40:39 -0500

    Time: 0 ms (100%), Space: 17.4 MB (5.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        closeToOpen = {")":"(","]":"[","}":"{"}
-        for c in s:
-            # if it is close 
-            if c in closeToOpen:
-                if stack and stack[-1]==closeToOpen[c]:
-                # stack and closing curly brace check if they are equal .
+        """ 
+        Check if the input string of parentheses is valid.
+        A valid string must have matching open and close parentheses in the correct order.
+
+        :param s: A string containing only '(',')','
+        """
+        matching_parentheses = {")": "(", "]": "[", "}": "{"}
+        stack=[]
+        for char in s:
+            stack.append(char)
+
+            if char in matching_parentheses:
+                if len(stack)>1 and stack[-2]==matching_parentheses[char]:
+                    stack.pop()
                     stack.pop()
                 else:
                     return False
-            # else if it is open add it to stack
-            else:
-                stack.append(c)
-        return True if not stack else False
             
+        return not stack
+        
