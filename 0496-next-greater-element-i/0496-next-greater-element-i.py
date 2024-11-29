87931cbcdb56fa90aa1ee5953c89a631cc153789
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-29 11:32:52 -0500

    Time: 0 ms (100%), Space: 17.5 MB (19.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
-        nums1_next_map={n:i for i,n in enumerate(nums1)}
-        result=[-1]*len(nums1)
+        nums1_index_map ={n:i for i,n in enumerate(nums1)}
+        res = [-1]*len(nums1)
+        stack = []
         for i in range(len(nums2)):
-            if nums2[i] not in nums1_next_map:
-                continue
-            for j in range(i+1,len(nums2)):
-                if nums2[j]>nums2[i]:
-                    inde=nums1_next_map[nums2[i]]
-                    result[inde]=nums2[j]
-                    break
-        return result
+            cur = nums2[i]
+            while stack and cur>stack[-1]:
+                val = stack.pop()
+                idx = nums1_index_map[val]
+                res[idx]=cur
+            if cur in nums1_index_map:
+                stack.append(cur)
+        return res
