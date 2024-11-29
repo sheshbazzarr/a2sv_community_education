61309d31804cc759712a422de9d11bf40fbc2121
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-29 17:50:41 -0500

    Time: 15 ms (96.55%), Space: 18.2 MB (5.63%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -1,13 +1,9 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        j = 0
-        s=list(s)
-        for i in range(len(s)):
-            # check if we have adjectnt duplicate
-            if j>0 and s[j-1]==s[i]:
-                j-=1
-                # we don't place the chr at the j point
+        stack = []
+        for char in s:
+            if stack and stack[-1]==char:
+                stack.pop()
             else:
-                s[j]=s[i]
-                j+=1
-        return ''.join(s[:j])
+                stack.append(char)
+        return ''.join(stack)
