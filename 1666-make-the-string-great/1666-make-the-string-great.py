35dd0dfa71295f0dcbab819b6af6f2de994aab36
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-30 07:21:36 -0500

    Time: 3 ms (49.64%), Space: 17.4 MB (9.12%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.py
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -2,8 +2,10 @@ class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
         for char in s:
-            if stack and ((char.isupper() and stack[-1].islower() and char.lower() == stack[-1]) or (char.islower() and stack[-1].isupper() and char.upper() == stack[-1])):
-                stack.pop()
+            if stack and ((char.isupper() and stack[-1].islower() and char.lower()==stack[-1])or(char.islower()and stack[-1].isupper() and char.upper()==stack[-1])):
+                 stack.pop()
             else:
                 stack.append(char)
         return ''.join(stack)
+
+
