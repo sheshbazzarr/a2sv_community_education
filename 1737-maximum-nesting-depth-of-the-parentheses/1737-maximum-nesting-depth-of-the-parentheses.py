5f29d1e2409671305cf9f63fdf4be72081bea18c
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-30 10:46:50 -0500

    Time: 0 ms (100%), Space: 17.2 MB (7.89%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        current_depth = 0
+        current_depth = 1
         max_depth = 0
         for char in s:
             if char =='(':
                 current_depth+=1
-                if current_depth>max_depth:
-                    max_depth = current_depth
             elif char==")":
                 current_depth-=1
+                max_depth=max(current_depth,max_depth)
         return max_depth
        
