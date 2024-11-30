bba479b12a3219ab736bf7ab38935ce75a34de23
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-30 08:38:06 -0500

    Time: 2 ms (57.42%), Space: 17.4 MB (9.12%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.py
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -1,11 +1,11 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         while True:
-            found =False
+            found =0
             for i in range(len(s)-1):
                 if s[i].swapcase()==s[i+1]:
                     s=s[:i]+s[i+2:]
-                    found =True
+                    found =1
                     break 
             if not found:
                 break
