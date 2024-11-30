b32f5b9ab897c86c34a0fb27a93fa9fadfc10366
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-30 08:37:06 -0500

    Time: 1 ms (64.71%), Space: 17.2 MB (9.12%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.py
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -1,13 +1,18 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        stack = []
+        while True:
+            found =False
+            for i in range(len(s)-1):
+                if s[i].swapcase()==s[i+1]:
+                    s=s[:i]+s[i+2:]
+                    found =True
+                    break 
+            if not found:
+                break
+        return s
 
-        for char in s:
-            if stack and char!=stack[-1] and char.swapcase()==stack[-1]:
-                stack.pop()
-            else:
-                stack.append(char)
-        return "".join(stack)
 
+
+        
 
     
