5b7397d193b1d667dbbdee2091b75f43cef8758f
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-11-30 20:47:06 -0500

    Time: 47 ms (75.4%), Space: 34.9 MB (35.66%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,33 +1,17 @@
 class Solution:
     def find132pattern(self, nums):
-        # Start by checking if the list has fewer than 3 elements
-        if len(nums) < 3:
-            # Can't have a 132 pattern with less than 3 numbers
+        if len(nums)<3:
             return False
-
-        # Initialize a stack to keep track of potential nums[j] values
         stack = []
+        second_number = float('-inf')
 
-        # This variable will keep track of the second number in the 132 pattern (nums[k])
-        second_number = float('-inf')  # Very small value, smaller than any number in the array
-
-        # We need to iterate through the list backward
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)-1,-1,-1):
             current_number = nums[i]
 
-            # Check if the current number is less than the second_number (nums[k])
-            if current_number < second_number:
-                # If true, we found the 132 pattern
+            if current_number<second_number:
                 return True
-
-            # Otherwise, we start updating the stack and second_number
-            # While there are numbers in the stack and the top of the stack is smaller than the current number
-            while stack and stack[-1] < current_number:
-                # Pop from the stack and set it as the second_number
+            
+            while stack and stack[-1]<current_number:
                 second_number = stack.pop()
-
-            # Push the current number onto the stack as a potential nums[j]
             stack.append(current_number)
-
-        # If we finish the loop and don't find a pattern, return False
         return False
