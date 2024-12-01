7b65a7584ce4e076ea4c2916754de3a0bfb35899
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-12-01 15:40:17 -0500

    Time: 1771 ms (13.48%), Space: 18.1 MB (11.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        StoreHash={} # value and index
-        for index ,value in enumerate(nums):
-            difference=target-value
-            if difference in StoreHash:
-                return [StoreHash[difference],index]
-            StoreHash[value]=index
-       
-      
+        for f1 in range(len(nums)):
+            for f2 in range(f1+1,len(nums)):
+                if (nums[f1]+nums[f2])==target:
+                    return [f1,f2]
+        
