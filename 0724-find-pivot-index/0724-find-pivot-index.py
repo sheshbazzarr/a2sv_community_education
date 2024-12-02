3483635881916ed0a8a0650cb5e0b3746a5e0a3d
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-12-02 03:54:15 -0500

    Time: 9 ms (37.73%), Space: 18.1 MB (7.87%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -3,13 +3,12 @@ class Solution:
         prefix_sum = [0]*(len(nums)+1)
         for i in range(len(nums)):
             prefix_sum[i+1]=prefix_sum[i]+nums[i]
-        total=prefix_sum
+        total=prefix_sum[-1]
         left_sum = 0
         for i ,num in enumerate(nums):
-            right_sum = total[-1]-left_sum -num
+            right_sum = total-left_sum -num
             if right_sum==left_sum:
                 return i
-
             left_sum+=num
         return -1
 
