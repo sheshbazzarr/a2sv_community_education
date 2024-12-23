9f87d860216782f00195c71f87a51cedfde9ebc2
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-12-23 03:39:14 -0500

    Time: 0 ms (100%), Space: 17.9 MB (5%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -1,11 +1,12 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
+        # Check if the length are different
         if len(s)!=len(goal):
             return False
-        length = len(s)
-        # Try all possible rotations of the string 
-        for x in range(length):
-            s=s[1:]+s[0]
-            if s==goal:
-                return True
-        return False
+        # create a new string by concatenating 's' with itself
+        doubled_string =s + s
+        # Use find to search for 'goal' in 'doubledString'
+        # If find returns an index that is not -1
+        # then 'goal' is a substring
+        return doubled_string.find(goal)!=-1
+
