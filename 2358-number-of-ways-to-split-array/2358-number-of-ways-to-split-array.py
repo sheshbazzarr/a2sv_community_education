5838de056e31b3820ec9c7c9fe40b840a7da28e1
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2025-01-05 19:08:58 -0500

    Time: 43 ms (91.05%), Space: 32.4 MB (68.73%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -1,12 +1,15 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
         
-        total_sum =sum(nums)
+        total_sum =sum(nums)   # time O(n)
         cnt=0
         left_sum=0
-        for i in nums[:-1]:
+        for i in nums[:-1]:   #----> space O(n) creat an array and time ->O(n)
             left_sum=i+left_sum
             balance=total_sum-left_sum
             if left_sum>=balance:
                 cnt+=1
         return cnt
+
+        # space : O(n)
+        # Time : O(n)
