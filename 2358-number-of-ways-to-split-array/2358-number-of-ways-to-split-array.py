266deec8a3e0cf095c7ceb112554753adb6b6eda
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2025-01-05 19:10:04 -0500

    Time: 53 ms (62.46%), Space: 32.4 MB (49.08%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -4,8 +4,8 @@ class Solution:
         total_sum =sum(nums)   # time O(n)
         cnt=0
         left_sum=0
-        for i in nums[:-1]:   #----> space O(n) creat an array and time ->O(n)
-            left_sum=i+left_sum
+        for i in range(len(nums)-1):   #----> space O(n) creat an array and time ->O(n)
+            left_sum+=nums[i]
             balance=total_sum-left_sum
             if left_sum>=balance:
                 cnt+=1
