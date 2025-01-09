313883db3078912b8985aed8e19903e5e8d73831
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2025-01-09 05:05:06 -0500

    Time: 0 ms (100%), Space: 17.9 MB (12.83%) - LeetHub

--- 0899-binary-gap/0899-binary-gap.py
+++ 0899-binary-gap/0899-binary-gap.py
@@ -4,5 +4,7 @@ class Solution:
         positions =[i for i, bit in enumerate(binary_number) if bit=='1']
         if len(positions)<2:
             return 0
-        max_=max(positions[i]-positions[i-1] for i in range(1, len(positions)))
+        max_ = 0
+        for i in range(1, len(positions)):
+            max_=max(max_,positions[i]-positions[i-1])
         return max_
